d94b5cf822b924b486d710844af51b52e3937195
JM Huang <fbihjmeric@gmail.com>
2018-07-10 23:54:48 +0800

    update README.md and config-example.go

--- README.md
+++ README.md
@@ -1,3 +1,28 @@
 # web-demo
+## pre-requirement
+### govendor https://github.com/kardianos/govendor
+### sql-migrate https://github.com/rubenv/sql-migrate
+
+## install dependency
+```
+$ govendor sync
+```
+
+## modify dbconfig.yml and update schema, ref: dbconfig-example.yml
+```
+$ vim ./dbconfig.yml
+$ sql-migrate up
+```
+## setup SQL Config, ref: config/config-exapmle.go
+```
+$ vim ./config/config.go
+```
 ## Run test
-GO_ENV=unit-test go test ./...
+```
+$ GO_ENV=unit-test go test ./...
+```
+
+## Run server
+```
+$ go run main.go
+```
---
+++ config/config-example.go
@@ -0,0 +1,33 @@
+package config
+
+// Config : Config
+// type Config struct {
+// 	DB *DBConfig
+// }
+
+// DBConfig : DBConfig
+// type DBConfig struct {
+// 	Dialect  string
+// 	Username string
+// 	Password string
+// 	IP       string
+// 	Port     string
+// 	Table    string
+// }
+
+// GetConfig
+// func GetConfigExample() *Config {
+// 	var config *Config
+// 	fmt.Print("GO_ENV is ", os.Getenv("GO_ENV"), ".\n")
+// 	config = &Config{
+// 		DB: &DBConfig{
+// 			Dialect:  "mysql",
+// 			Username: "demo",
+// 			Password: "",
+// 			IP:       "",
+// 			Port:     "3306",
+// 			Table:    "webdemo",
+// 		},
+// 	}
+// 	return config
+// }
